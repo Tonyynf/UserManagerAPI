5733b0990628497abc5f3d2d92b74c785c5b727d
Tonyynf <tobyencarnacao@gmail.com>
2025-03-27 19:49:34 -0300

    instalando dependencias

--- UserManagerAPI/obj/Debug/net9.0/UserManagerAPI.AssemblyInfo.cs
+++ UserManagerAPI/obj/Debug/net9.0/UserManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95c0df7d8839f591e4d0cc8334847e5786ab00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
