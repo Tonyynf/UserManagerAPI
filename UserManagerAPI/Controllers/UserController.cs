9f8f483f66e4b393afa30841e4e82f79780db410
Tonyynf <tobyencarnacao@gmail.com>
2025-03-31 19:11:00 -0300

    Adicionando CRUD no UserController.cs

--- UserManagerAPI/Controllers/UserController.cs
+++ UserManagerAPI/Controllers/UserController.cs
@@ -1,17 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
 using UserManagerAPI.Models;
+using UserManagerAPI.Services;
+
+namespace UserManagerAPI.Controllers;
 
 [ApiController]
-[Route("api/users")]
-public class UserController : ControllerBase{
+[Route("api/users")] 
+public class UserController : ControllerBase
+{
     private readonly UserService _userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService)
+    {
         _userService = userService;
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] User user){
+    public IActionResult CreateUser([FromBody] User user)
+    {
         var createdUser = _userService.CreateUser(user);
-        return CreatedAtAction(nameof(GetUser), new{ id = createdUser.Id}, createdUserUser);
+        return CreatedAtAction(nameof(CreateUser), new { id = createdUser.Id }, createdUser);
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = _userService.GetUsers();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetUserById(int id)
+    {
+        var user = _userService.GetUserById(id);
+        if (user == null)
+            return NotFound(new { message = "Usuário não encontrado!" });
+
+        return Ok(user);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, [FromBody] User user)
+    {
+        var updatedUser = _userService.UpdateUser(id, user);
+        if (updatedUser == null)
+            return NotFound(new { message = "Usuário não encontrado!" });
+
+        return Ok(updatedUser);
+    }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        var deleted = _userService.DeleteUser(id);
+        if (!deleted)
+            return NotFound(new { message = "Usuário não encontrado!" });
+
+        return NoContent();
     }
 }
